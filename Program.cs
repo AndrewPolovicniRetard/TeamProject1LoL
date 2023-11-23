9df6c28214a46cb1d407a147405d29e363e1741a
AndrewPolovicniRetard <1155@student.itgymnazium.cz>
2023-11-23 10:29:33 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamProjectCsNejlepsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tady kod zmrdi");
+        }
+    }
+}
