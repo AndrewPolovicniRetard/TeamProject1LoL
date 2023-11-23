b0fcf72fde8bd7308622c29f3d0f7bbee29d7398
AndrewPolovicniRetard <1155@student.itgymnazium.cz>
2023-11-23 11:59:38 +0100

    Everything commit 2.0

--- Program.cs
+++ Program.cs
@@ -9,8 +9,11 @@ namespace TeamProjectCsNejlepsi
             bool proceed = true;
             
             string finalSentence = "";
-
+            Console.WriteLine("<Zadejte vas vyber, pro 1 az 3 zadani(1/2/3)");
+            Console.WriteLine("<PS: 2 zadani jsem rozdelil na 2 ruzne");
+            Console.WriteLine("");
             string userInputChoice = Console.ReadLine();
+            Console.Clear();
             if (userInputChoice == "1")
             {
                 while (proceed)
