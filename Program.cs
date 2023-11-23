947b68e7fa6652a6f5a261939caecc74d6c4464e
AndrewPolovicniRetard <1155@student.itgymnazium.cz>
2023-11-23 11:49:51 +0100

    Everything commit

--- Program.cs
+++ Program.cs
@@ -10,30 +10,62 @@ namespace TeamProjectCsNejlepsi
             
             string finalSentence = "";
 
-            while (proceed)
+            string userInputChoice = Console.ReadLine();
+            if (userInputChoice == "1")
+            {
+                while (proceed)
+                {
+
+                    if (proceed == true)
+                    {
+                        Console.WriteLine("<Zadejte slovo:");
+
+                    }
+                    string userInputA = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(userInputA))
+                    {
+                        Console.WriteLine(finalSentence);
+                        proceed = false;
+                    }
+                    else if (!string.IsNullOrWhiteSpace(userInputA))
+                    {
+                        finalSentence += userInputA;
+
+                    }
+                }
+            } else if (userInputChoice == "2")
+            {
+                int counter = 0;
+                Console.WriteLine("<Zadejte slovo, ktere chcete zopakovat 5krat:");
+                string userInputB = Console.ReadLine();
+                while (proceed)
+                {
+                    
+                    if (counter != 5){
+                        Console.WriteLine(userInputB);
+                        counter++;
+                    }
+                    else
+                    {
+                        proceed = false;
+                    }
+                }
+                
+
+            } else if (userInputChoice == "3")
             {
 
-                if (proceed == true)
+                for (int i = 1; i < int.MaxValue; i++)
                 {
-                    Console.WriteLine("<Zadejte slovo:");
-                    
-                }
-                string userInputA = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(userInputA))
-                {
-                    Console.WriteLine(finalSentence);
-                    proceed = false;
-                }
-                else if (!string.IsNullOrWhiteSpace(userInputA))
-                {
-                    finalSentence += userInputA;
-                
+                    Console.WriteLine("<Zadejte slovo pro opakovani:");
+                    string userInput2 = Console.ReadLine();
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.WriteLine(userInput2);
+                    }
                 }
             }
-           
-
-            
         }
     }
 }
